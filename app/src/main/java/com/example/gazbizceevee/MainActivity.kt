c36269f623ec4551c3076577d516cdb494f6e7f4
Gaz Biz <gareth@resdiary.com>
2023-05-10 12:05:31 +0100

    Set up nav, create bottom tabs

--- app/src/main/java/com/example/gazbizceevee/MainActivity.kt
+++ app/src/main/java/com/example/gazbizceevee/MainActivity.kt
@@ -2,17 +2,35 @@ package com.example.gazbizceevee
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.core.net.toUri
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.gazbizceevee.ui.theme.GazBizCeeVeeTheme
 import com.rizzi.bouquet.ResourceType
 import com.rizzi.bouquet.VerticalPDFReader
@@ -21,47 +39,97 @@ import java.io.File
 
 class MainActivity : ComponentActivity() {
 
+    private val navigationItems = listOf(
+        Screen.Home,
+        Screen.Experience,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            val file = getFileFromAssets(this, "cv.pdf")
-            Log.e("MainActivity", "File exists: ${file.toURI()}")
         setContent {
-            val pdfState = rememberVerticalPdfReaderState(
-                resource = ResourceType.Local(file.toUri()),
-                isZoomEnable = true
-            )
-
-            GazBizCeeVeeTheme {
-                Surface(
-                    color = MaterialTheme.colorScheme.background
+            Scaffold(
+                bottomBar = { createBottomNav(rememberNavController()) }
+            ) { innerPadding ->
+                NavHost(
+                    rememberNavController(),
+                    startDestination = Screen.Home.route,
+                    Modifier.padding(innerPadding)
                 ) {
-                    VerticalPDFReader(
-                        state = pdfState,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = MaterialTheme.colorScheme.background)
-                    )
+                    composable(Screen.Home.route) { Home() }
+                    composable(Screen.Experience.route) { Experience() }
                 }
             }
         }
     }
 
-    private fun getFileFromAssets(context: Context, fileName: String): File = File(context.cacheDir, fileName)
-        .also {
-            if (!it.exists()) {
-                it.outputStream().use { cache ->
-                    context.assets.open(fileName).use { inputStream ->
-                        inputStream.copyTo(cache)
+    @Composable
+    private fun createBottomNav(navController: NavHostController) {
+        BottomNavigation {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+            navigationItems.forEach { screen ->
+                BottomNavigationItem(
+                    icon = { Icon(screen.icon, contentDescription = null) },
+                    label = { Text(stringResource(screen.resourceId)) },
+                    selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Home() {
+        val file = getFileFromAssets(this@MainActivity, "cv.pdf")
+        val pdfState = rememberVerticalPdfReaderState(
+            resource = ResourceType.Local(file.toUri()),
+            isZoomEnable = true
+        )
+
+        GazBizCeeVeeTheme {
+            Surface(
+                color = MaterialTheme.colorScheme.background
+            ) {
+                VerticalPDFReader(
+                    state = pdfState,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = MaterialTheme.colorScheme.background)
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun Experience() {
+        Text(text = "This will contain the experience section of the CV.")
+    }
+
+    private fun getFileFromAssets(context: Context, fileName: String): File =
+        File(context.cacheDir, fileName)
+            .also {
+                if (!it.exists()) {
+                    it.outputStream().use { cache ->
+                        context.assets.open(fileName).use { inputStream ->
+                            inputStream.copyTo(cache)
+                        }
                     }
                 }
             }
-        }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        GazBizCeeVeeTheme {
-
-        }
-    }
 }
+
+
+sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: ImageVector) {
+    object Home : Screen("home", R.string.home, Icons.Filled.Home)
+    object Experience : Screen("experience", R.string.experience, Icons.Filled.List)
+}
+
+
